f44ec23f748e8089b9e4435baf6c592ba2a874ec
Juda Buchahda <miminerd06@gmail.com>
2025-04-10 18:04:21 +0200

    feat(app/src): Heading cmp

---
+++ read-me/src/app/components/heading-section/heading/heading.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { HeadingFormatPipe } from '../../../pipes/heading-formatter-pipe';
+import { headingType } from '../../../types/types';
+
+@Component({
+  selector: 'app-heading',
+  standalone: true,
+  imports: [HeadingFormatPipe],
+  templateUrl: './heading.component.html',
+  styleUrl: './heading.component.scss',
+})
+export class HeadingComponent {
+  text = 'This is a heading';
+  selectedHeading: headingType = 'h3';
+}
