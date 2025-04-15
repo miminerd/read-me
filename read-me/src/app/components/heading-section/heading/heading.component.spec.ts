6bae117ea1641c6690d7b56b6460975203077940
Juda Buchahda <miminerd06@gmail.com>
2025-04-15 11:02:20 +0200

    feat(app/src): heading cmp

--- read-me/src/app/components/heading-section/heading/heading.component.spec.ts
+++ read-me/src/app/components/heading-section/heading/heading.component.spec.ts
@@ -1,23 +1,22 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { HeadingComponent } from './heading.component';
+import { HeadingComponent } from "./heading.component";
 
-describe('HeadingComponent', () => {
-  let component: HeadingComponent;
-  let fixture: ComponentFixture<HeadingComponent>;
+describe("HeadingComponent", () => {
+	let component: HeadingComponent;
+	let fixture: ComponentFixture<HeadingComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [HeadingComponent]
-    })
-    .compileComponents();
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [HeadingComponent],
+		}).compileComponents();
 
-    fixture = TestBed.createComponent(HeadingComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+		fixture = TestBed.createComponent(HeadingComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it("should create", () => {
+		expect(component).toBeTruthy();
+	});
 });
--- read-me/src/app/components/heading-section/heading/heading.component.ts
+++ read-me/src/app/components/heading-section/heading/heading.component.ts
@@ -1,15 +1,15 @@
-import { Component } from '@angular/core';
-import { HeadingFormatPipe } from '../../../pipes/heading-formatter-pipe';
-import { headingType } from '../../../types/types';
+import { Component } from "@angular/core";
+import { HeadingFormatPipe } from "../../../pipes/heading-formatter-pipe";
+import { headingType } from "../../../types/types";
 
 @Component({
-  selector: 'app-heading',
-  standalone: true,
-  imports: [HeadingFormatPipe],
-  templateUrl: './heading.component.html',
-  styleUrl: './heading.component.scss',
+	selector: "app-heading",
+	standalone: true,
+	imports: [HeadingFormatPipe],
+	templateUrl: "./heading.component.html",
+	styleUrl: "./heading.component.scss",
 })
 export class HeadingComponent {
-  text = 'This is a heading';
-  selectedHeading: headingType = 'h3';
+	text = "This is a heading";
+	selectedHeading: headingType = "h3";
 }
